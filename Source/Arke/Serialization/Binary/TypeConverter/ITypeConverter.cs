01ad52d2a58c20e66d96ca81a1e6cc8cb20584c7
dbailey <dbailey@kleisden.com>
2016-10-06 15:08:35 -0400

    added type converter

---
+++ Source/Arke/Serialization/Binary/TypeConverter/ITypeConverter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Arke.Serialization.Binary.TypeConverter
+{
+    interface ITypeConverter<T>
+    {
+        byte[] Serialize(T obj);
+
+        T Deserialize(byte[] stream, int position, out int length);
+    }
+}
