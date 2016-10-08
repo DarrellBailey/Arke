8ef770cf5ff72234fc86b99b60e7bce581068ddf
Darrell Bailey <darrell.bailey@cinemassive.com>
2016-10-08 15:06:15 -0400

    got a library that will actually let me do the damn binary serialization...

--- Source/Arke/Serialization/Binary/BinarySerializer.cs
+++ Source/Arke/Serialization/Binary/BinarySerializer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace Arke.Serialization.Binary
@@ -14,7 +15,28 @@ namespace Arke.Serialization.Binary
 
         public byte[] Serialize(object deserialized)
         {
-            throw new NotImplementedException();
+            PropertyInfo[] properties = GetObjectProperties(deserialized);
+
+            List<byte> serialized = new List<byte>();
+
+            for(int i = 0; i < properties.Length; i++)
+            {
+                byte[] bytes = GetPropertyBytes(deserialized, properties[i]);
+
+                serialized.AddRange(bytes);
+            }
+
+            return serialized.ToArray();
+        }
+
+        private PropertyInfo[] GetObjectProperties(object obj)
+        {
+            return obj.GetType().GetProperties(BindingFlags.Public).Where(x => x.CanRead && x.CanWrite).OrderBy(x => x.Name).ToArray();
+        }
+
+        private byte[] GetPropertyBytes(object obj, PropertyInfo propertyInfo)
+        {
+            return new byte[0];
         }
     }
 }
