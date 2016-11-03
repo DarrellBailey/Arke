5da3edc85bf8184089b475b7edd00ed6474a9c0b
dbailey <dbailey@kleisden.com>
2016-11-03 10:27:41 -0400

    got basic get orking with json

--- Source/Arke/Api/ContentProcessors/JsonContentProcessor.cs
+++ Source/Arke/Api/ContentProcessors/JsonContentProcessor.cs
@@ -1,21 +1,32 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Arke.Api.ContentProcessors
 {
     public class JsonContentProcessor : IContentProcessor
     {
-        public Task<T> ProcessContent<T>(HttpContent content)
+        public async Task<T> ProcessContent<T>(HttpContent content)
         {
-            throw new NotImplementedException();
+            string json = await content.ReadAsStringAsync();
+
+            return JsonConvert.DeserializeObject<T>(json);
         }
 
-        public Task<HttpContent> ProcessObject(object obj)
+        public async Task<HttpContent> ProcessObject(object obj)
         {
-            throw new NotImplementedException();
+            string serialized = JsonConvert.SerializeObject(obj);
+
+            byte[] bytes = Encoding.UTF8.GetBytes(serialized);
+
+            MemoryStream stream = new MemoryStream(bytes);
+
+            return new StreamContent(stream);
         }
     }
 }
---
+++ Tests/Arke.Tests/Api/ArkeApiClientTests.cs
@@ -0,0 +1,47 @@
+﻿using Arke.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Arke.Tests.Api
+{
+    public class ArkeApiClientTests
+    {
+        private ArkeApiClient client = new ArkeApiClient(); 
+
+        public ArkeApiClientTests()
+        {
+            ArkeApiConfiguration.Default.TypeBindings.Add(typeof(NasaPictureOfTheDay), "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY");
+        }
+
+        [Fact]
+        public async void CanGet()
+        {
+            NasaPictureOfTheDay pod = await client.Get<NasaPictureOfTheDay>();
+
+            Assert.NotNull(pod);
+        }
+
+        public class NasaPictureOfTheDay
+        {
+            public string copyright { get; set; }
+
+            public DateTime date { get; set; }
+
+            public string explanation { get; set; }
+
+            public string hdurl { get; set; }
+
+            public string media_type { get; set; }
+
+            public string service_version { get; set; }
+
+            public string title { get; set; }
+
+            public string url { get; set; }
+        }
+
+    }
+}
